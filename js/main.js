45152c345367f0d8456598978256f41d25616628
James Croft <james.croft@gmail.com>
2020-07-12 15:50:10 +1000

    remove test alert

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ $('.navbar-toggler a.search-link').on('click', function () {
 });
 
 //iOS version detection
-
 function iOSversion() {
     if (/iP(hone|od|ad)/.test(navigator.platform)) {
       // supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
@@ -16,9 +15,9 @@ function iOSversion() {
       return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
     }
   }
-  
+
   ver = iOSversion();
-  
-  if (ver[0] != null) {
-    alert("This is running iOS version: " + ver );
+  if (ver != null) {
+    console("This is running iOS version: " + ver);
   }
+
