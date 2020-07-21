58b44fc7aaafb00b1e9b02dceb9fbeaaca6a4a62
James Croft <james.croft@gmail.com>
2020-07-21 16:00:41 +1000

    fix for top ad, fix search

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@
 var gulp = require('gulp');
 var mustache = require('gulp-mustache');
 
+gulp.task('default', function() {
+    return gulp.watch('templates/index.html', gulp.series('mustache'));
+});
+
 gulp.task('mustache', async function() {
     gulp.src('templates/*')
         .pipe(mustache('data/emoji-v13-categories.json'))
--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 
 $(document).ready(function () {
 
-
   //Hide navbar on search
   $('.navbar-toggler a.search-link').on('click', function () {
     $('.emoji-navlinks').toggleClass('show')
@@ -12,12 +11,32 @@ $(document).ready(function () {
   var options = {
     valueNames: ['emoji-symbol', 'emoji-description', 'emoji-tags']
   };
-  var emojiList = new List('emoji', options);
+  var emojiListSmileysEmotion = new List('smileys-&-emotion', options);
+  var emojiListPeopleBody = new List('people-&-body', options);
+  var emojiListAnimalsNature = new List('animals-&-nature', options);
+  var emojiListFoodDrink = new List('food-&-drink', options);
+  var emojiListTravelPlaces = new List('travel-&-places', options);
+  var emojiListActivities = new List('activities', options);
+  var emojiListObjects = new List('objects', options);
+  var emojiListSymbols = new List('symbols', options);
+  var emojiListFlags = new List('flags', options);
 
   // find search for list
   $('#search-field').on('keyup', function (e) {
+    $('.cat-divider').hide();
+    $('.row h2').hide();
     var searchString = $(this).val();
-    emojiList.search(searchString);
+
+    emojiListSmileysEmotion.search(searchString);
+    emojiListPeopleBody.search(searchString);
+    emojiListAnimalsNature.search(searchString);
+    emojiListFoodDrink.search(searchString);
+    emojiListTravelPlaces.search(searchString);
+    emojiListActivities.search(searchString);
+    emojiListObjects.search(searchString);
+    emojiListSymbols.search(searchString);
+    emojiListFlags.search(searchString);
+
     if (e.which == 13) {
       $(this).blur();
       $('#navbarSearch').toggleClass('show');
@@ -30,7 +49,17 @@ $(document).ready(function () {
     $('#navbarSearch').toggleClass('show');
     $('.emoji-navlinks').toggleClass('show');
     $('#search-field').val("");
-    emojiList.search($('#search-field').val());
+    emojiListSmileysEmotion.search($('#search-field').val());
+    emojiListPeopleBody.search($('#search-field').val());
+    emojiListAnimalsNature.search($('#search-field').val());
+    emojiListFoodDrink.search($('#search-field').val());
+    emojiListTravelPlaces.search($('#search-field').val());
+    emojiListActivities.search($('#search-field').val());
+    emojiListObjects.search($('#search-field').val());
+    emojiListSymbols.search($('#search-field').val());
+    emojiListFlags.search($('#search-field').val());
+    $('.cat-divider').show();
+    $('.row h2').show();
   });
 
   // initialise clipboard.js
